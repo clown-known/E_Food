ba7266acfc7b7e292bc871401af13df6a57d5afe
jasonmawr <105283127+jasonmawr@users.noreply.github.com>
2023-06-13 23:13:34 +0700

    RestaurantApi [HaiCuong]

--- EXE02_EFood/EXE02_EFood_API/Startup.cs
+++ EXE02_EFood/EXE02_EFood_API/Startup.cs
@@ -1,16 +1,21 @@
+using EXE02_EFood_API.Models;
+using EXE02_EFood_API.Repository;
+using EXE02_EFood_API.Repository.IRepository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging;                                    
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 
 namespace EXE02_EFood_API
 {
@@ -26,12 +31,16 @@ namespace EXE02_EFood_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<E_FoodContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DbConnect")));
             services.AddControllers();
+            services.AddScoped<IRestaurantRepository, RestaurantRepositoryImp>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "EXE02_EFood_API", Version = "v1" });
             });
+            //services.AddSwaggerGen();
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,10 +51,11 @@ namespace EXE02_EFood_API
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EXE02_EFood_API v1"));
+                //app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
-
+            
             app.UseRouting();
 
             app.UseAuthorization();
